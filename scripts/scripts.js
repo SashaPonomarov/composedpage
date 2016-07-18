89815aa8d79adb36ac61099a0f12a9065aacb05b
Sasha Ponomarov <randirsbox@gmail.com>
2016-07-18 15:14:09 +0300

    Adds jQuery-menu-aim plugin for hover intent on menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,22 +6,36 @@ $( document ).ready(function() {
         $( this ).css("background-color", "rgba(0, 0, 0, 0.05)")
         $( this ).children(".header-menu-sublist").slideToggle(200)
     })
-    $(".header-menu-sublist li").hover(
-        function(){
-            $( this ).children(".header-menu-sublist").show()
-        },
-        function(){
-            $( this ).children(".header-menu-sublist").hide()
-        }
-    )
+
     $(".header-menu").click(function(e){
         e.stopPropagation();
     })
     $(".header-menu-sublist").click(function(e){
         e.stopPropagation();
     })
-    $(document).click(function() {
-        $(".header-menu-sublist").slideUp(200)
-        $(".header-menu>li").css("background-color", "rgb(256, 256, 256)")
+    
+    $(document).click(
+            closeAllSubmenues()
+        )
+    
+    $(document).keyup(function(e) {
+            if (e.keyCode == 27) closeAllSubmenues()
+        })
+
+    $(".header-menu-sublist").menuAim({
+        activate: openSubmenu,
+        deactivate: closeSubmenu
     })
 })
+
+function openSubmenu(row) {
+    $( row ).children(".header-menu-sublist").show()
+}
+function closeSubmenu(row) {
+    $( row ).children(".header-menu-sublist").hide()
+}
+function closeAllSubmenues() {
+    $(".header-menu-sublist .header-menu-sublist").hide()
+    $(".header-menu-sublist").slideUp(200)
+    $(".header-menu>li").css("background-color", "rgb(256, 256, 256)")
+}
